28d0b32cff4191547a92ea8df9af8d9059927be2
Mitchell Vitez <mitchellvitez@gmail.com>
2014-04-09 17:59:04 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    
+<head>
+  <meta charset="utf-8" />
+  <title>Empty project</title>
+
+  <style>
+body {
+	margin: 0;
+}
+    #game_div {
+      width: 100%;
+      margin: 0;
+    }
+  </style>
+
+  <script type="text/javascript" src="phaser.min.js"></script>
+  <script type="text/javascript" src="main.js"></script>
+</head>
+
+<body>
+
+  <div id="game_div"> </div>
+
+</body>
+
+</html>
